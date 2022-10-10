5c3641c632c46108a2ed0fc44c50cad4033e78fb
Crank <1300833701@qq.com>
2022-10-10 18:32:02 +0800

    尝试判断参数为空静态方法。

---
+++ WorkDaKa/src/main/java/com/example/workdaka/utils/EmptyUtils.java
@@ -0,0 +1,12 @@
+package com.example.workdaka.utils;
+
+public class EmptyUtils {
+
+    public static boolean StringEmptyUtils(String string){
+        if(string.equals("") || string == null){
+            return true;
+        }
+        return false;
+    }
+
+}
